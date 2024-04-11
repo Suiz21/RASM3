3925685447d7b56bad2de8e392553985e88911ea
Suiz21 <147289136+Suiz21@users.noreply.github.com>
2024-04-11 15:18:42 -0700

    Update startsWith1.s

--- startsWith1.s
+++ startsWith1.s
@@ -1,3 +1,17 @@
+/* Programmer: Aidan Ramirez
+ * startsWith_2 (RASM_3)
+ * Purpose: return true or false statement whether a string starts with a certain function, at a given index
+ * Author: Aidan Ramirez
+ * Date last modified: 11 April 2024
+ */
+
+// Contract for startsWith_2:
+        // Subroutine length: Provided a pointer to a null terminated string, startsWith_2 will return a true or false based on the parameters
+        // X0: Must point to a null terminated string
+        // LR: Must contain the return address
+        // All AAPCS required registers are preserved,  r19-r29 and SP.
+
+
 .global startsWith_1
 
   .data
@@ -8,45 +22,45 @@
   .text
   startsWith_1:
   
-  STR X30,[SP, # -16]!
+  STR X30,[SP, # -16]!          // push registers
   STR X0,[SP,#-16]!
   STR X1,[SP,#-16]!
 
 
-  ADD X0,X0,X4 
+  ADD X0,X0,X4          // push to correct byte
 
   loop:
-  LDRB W2,[x0]
-  LDRB w3,[x1]
+  LDRB W2,[x0]          // load byte
+  LDRB w3,[x1]          // load byte
 
-  CMP W3,0x00
-  BEQ equal
+  CMP W3,0x00           // compare to null
+  BEQ equal             // branch if equl
 
-  CMP W2, W3
-  BNE not_equal
+  CMP W2, W3            // compare bytes
+  BNE not_equal         // branch if not equal
 
-  ADD  X0, X0,#1
-  ADD  X1, X1,#1
+  ADD  X0, X0,#1        // increment
+  ADD  X1, X1,#1        // increment
 
 
-  B loop
+  B loop                // branch
 
   equal:
-  LDR X0,=EqualStr
-  BL  putstring
-  LDR X0,=chLF
-  BL  putch
-  B   done 
+  LDR X0,=EqualStr      // load
+  BL  putstring         // print
+  LDR X0,=chLF          // load
+  BL  putch             // print
+  B   done              // finish
 
   not_equal:
-  LDR X0,=NotEqualStr
-  BL  putstring
-  LDR X0,=chLF
-  BL  putch
-  B   done
+  LDR X0,=NotEqualStr   // load 
+  BL  putstring         // print
+  LDR X0,=chLF          // load
+  BL  putch             // print
+  B   done              // finsih
 
  done:
- LDR X1,[SP], #16
+ LDR X1,[SP], #16       // pop registers back
  LDR X0,[SP], #16
  LDR X30,[SP], #16
 
