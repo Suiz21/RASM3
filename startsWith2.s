062e974591083a1e6536592e83d1f90fcec19fab
Suiz21 <147289136+Suiz21@users.noreply.github.com>
2024-04-10 23:32:35 -0700

    Create startsWith2.s

---
+++ startsWith2.s
@@ -0,0 +1,55 @@
+.global startsWith_2
+          .equ BUFFER, 21                               // Variable that will be used to allocate space
+          .equ MAX_LEN,20                               // Creates a restriction on the length of inputs
+
+  .data
+    EqualStr:       .asciz "True" 
+    NotEqualStr:    .asciz "False" 
+    chLF:           .byte 0xa                                    // (NL line feed, new line)
+
+  .text
+  startsWith_2:
+  
+  STR X30,[SP, # -16]!
+  STR X0,[SP,#-16]!
+  STR X1,[SP,#-16]!
+ 
+  loop:
+  LDRB w2,[x0]
+  LDRB w3,[x1]
+
+  CMP W3,0x00
+  BEQ equal
+
+  CMP W2, W3
+  BNE not_equal
+
+  ADD  X0, X0,#1
+  ADD  X1, X1,#1
+
+
+  B loop
+
+  equal:
+  LDR X0,=EqualStr
+  BL  putstring
+  LDR X0,=chLF
+  BL  putch
+  B   done 
+
+  not_equal:
+  LDR X0,=NotEqualStr
+  BL  putstring
+  LDR X0,=chLF
+  BL  putch
+  B   done
+
+ done:
+ LDR X1,[SP], #16
+ LDR X0,[SP], #16
+ LDR X30,[SP], #16
+
+ RET LR
+
+
+.end
