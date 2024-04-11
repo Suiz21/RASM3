e060090bbf0a114dd10b890c48c5beffa4699cc2
Suiz21 <147289136+Suiz21@users.noreply.github.com>
2024-04-11 15:15:11 -0700

    Update copy.s

--- copy.s
+++ copy.s
@@ -1,3 +1,18 @@
+/* Programmer: Aidan Ramirez
+ * copy (RASM_3)
+ * Purpose: Copies the string given
+ * Author: Aidan Ramirez
+ * Date last modified: 11 April 2024
+ */
+
+// Contract for String_length:
+        // Subroutine length: Provided a pointer to a null terminated string, String_length will 
+        //      return the length of the string in X0
+        // X0: Must point to a null terminated string
+        // LR: Must contain the return address
+        // All AAPCS required registers are preserved,  r19-r29 and SP.
+
+
 .global copy
 
     .data
@@ -21,42 +36,42 @@ copy:
 loop1_copy:
 
     add    x2,x2,#1                // Increment x2 by 1
-    ldrb    w1,[x7],#1            // Set w1 = next character in string
-    cmp    w1,#0                    // Check if w1 == null character
-    beq    exit_copy            // Branch to exit as w1 is the null character
-    b        loop1_copy            // Repeat the loop until the null character is found
+    ldrb    w1,[x7],#1            // Set w1 = next character in string, increment
+    cmp    w1,#0                    // Check if w1 is null
+    beq    exit_copy            // Branch to exit  if the null character
+    b        loop1_copy            // loop
 
 exit_copy:
 
-    mov    x4,x2
+    mov    x4,x2             //
 
-    str    x0,[SP,#-16]!        // String to copy
-    str    x4,[SP,#-16]!        // Length of string
-    str    x30,[SP,#-16]!
+    str    x0,[SP,#-16]!        // push
+    str    x4,[SP,#-16]!        // push
+    str    x30,[SP,#-16]!       // push
 
     mov    x0,x2                    // Set x0 = x2, length of string
     bl        malloc                // Allocate enough bytes for length of string
 
-    ldr    x1,=ptrString
-    str    x0,[x1]
+    ldr    x1,=ptrString         // load 
+    str    x0,[x1]               // store x0
 
-    ldr    x30,[SP],#16
-    ldr    x4,[SP],#16
+    ldr    x30,[SP],#16    // pop
+    ldr    x4,[SP],#16    // pop
     ldr    x0,[SP],#16
 
-    ldr    x1,=ptrString
-    ldr    x1,[x1]
-    mov    x3,#0
+    ldr    x1,=ptrString  // load 
+    ldr    x1,[x1]        // load into x1
+    mov    x3,#0          // reset ciybter
 
 loop2_copy:
 
-    ldrb    w2,[x0,x3]
-    strb    w2,[x1,x3]
-    add    x3,x3,#1
-    cmp    x3,x4
-    bne    loop2_copy
+    ldrb    w2,[x0,x3]        // load byte, increment 3
+    strb    w2,[x1,x3]        // load byte, icnrement 3
+    add    x3,x3,#1           // increment 3
+    cmp    x3,x4              // check if x3 equals 4
+    bne    loop2_copy         // if not, repeat
 
-    mov    x0,x1
+    mov    x0,x1              // copy register
 
     RET    LR
 
