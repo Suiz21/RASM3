77035a2d9dd3081c4ce831dd67dfdcc0a18c8d65
Suiz21 <147289136+Suiz21@users.noreply.github.com>
2024-04-11 15:18:04 -0700

    Update equal.s

--- equal.s
+++ equal.s
@@ -1,10 +1,17 @@
-// PseudoCode:
-// Compare lengths to begin with. 
-// Go byte by byte comparing the value
-// If both always have equal bytes (cspr is 0), return true
-// Else, return false.
+/* Programmer: Aidan Ramirez
+ * equal (RASM_3)
+ * Purpose: Determines if 2 strings are equal
+ * Author: Aidan Ramirez
+ * Date last modified: 11 April 2024
+ */
 
-  .global Equals
+// Contract for equal:
+        // Subroutine length: Provided a pointer to a null terminated string, equals will 
+        //      return a bool whether the value is true or not
+        // X0: Must point to a null terminated string
+        // LR: Must contain the return address
+        // All AAPCS required registers are preserved,  r19-r29 and SP.
+ .global Equals
           .equ BUFFER, 21                               // Variable that will be used to allocate space
           .equ MAX_LEN,20                               // Creates a restriction on the length of inputs
 
@@ -16,42 +23,42 @@
   .text
   Equals:
   
-  STR X30,[SP, # -16]!
+  STR X30,[SP, # -16]!  // push registrs
   STR X0,[SP,#-16]!
   STR X1,[SP,#-16]!
  
   loop:
-  LDRB w2,[x0]
-  LDRB w3,[x1]
+  LDRB w2,[x0]  // load byte
+  LDRB w3,[x1]  // load byte
 
-  CMP W2,0x00
-  BEQ equal
+  CMP W2,0x00  // comapre to null
+  BEQ equal     // branch to equal if equal
 
-  CMP W2, W3
-  BNE not_equal
+  CMP W2, W3    // compare bytes
+  BNE not_equal // branch to not_equal if not equal
 
-  ADD  X0, X0,#1
-  ADD  X1, X1,#1
+  ADD  X0, X0,#1   // increment
+  ADD  X1, X1,#1   // increment
 
 
-  B loop
+  B loop        // loop
 
   equal:
-  LDR X0,=EqualStr
-  BL  putstring
-  LDR X0,=chLF
-  BL  putch
-  B   done 
+  LDR X0,=EqualStr      // load
+  BL  putstring         /// print
+  LDR X0,=chLF          // load
+  BL  putch             // print
+  B   done              // finish
 
   not_equal:
-  LDR X0,=NotEqualStr
-  BL  putstring
-  LDR X0,=chLF
-  BL  putch
-  B   done
+  LDR X0,=NotEqualStr   // load 
+  BL  putstring         // print
+  LDR X0,=chLF          // load
+  BL  putch             // print
+  B   done              // finish
 
  done:
- LDR X1,[SP], #16
+ LDR X1,[SP], #16       // pop registers
  LDR X0,[SP], #16
  LDR X30,[SP], #16
 
